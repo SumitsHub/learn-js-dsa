8b09c78be4759de46e9561777b78f52092e4b574
Sumit Paul <pauls1317@gmail.com>
2023-10-15 16:43:49 +0530

    creating class in JS

---
+++ 02_DSA_Udemy/class.js
@@ -0,0 +1,16 @@
+class Cookie {
+  constructor(color) {
+    this.color = color;
+  }
+  getColor() {
+    return this.color;
+  }
+  setColor(color) {
+    this.color = color;
+  }
+}
+
+let cookieOne = new Cookie('green');
+let cookieTwo = new Cookie('blue');
+
+const list = Map();
