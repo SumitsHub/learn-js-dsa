2fbd04031c4f18c714d7a40681287de91a352dcf
Sumit Paul <pauls1317@gmail.com>
2023-10-16 14:18:48 +0530

    LL: question #1-findMiddleNode

---
+++ 02_DSA_Udemy/01_LinkedList/02-questions/01-findMiddleNode.js
@@ -0,0 +1,115 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+  }
+
+  printList() {
+    let temp = this.head;
+    while (temp !== null) {
+      console.log(temp.value);
+      temp = temp.next;
+    }
+  }
+
+  getHead() {
+    if (this.head === null) {
+      console.log('Head: null');
+    } else {
+      console.log('Head: ' + this.head.value);
+    }
+  }
+
+  getTail() {
+    if (this.tail === null) {
+      console.log('Tail: null');
+    } else {
+      console.log('Tail: ' + this.tail.value);
+    }
+  }
+
+  makeEmpty() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  /** find middle node of the linked list */
+  findMiddleNode() {
+    // pointer to move single step
+    let slow = this.head;
+    // pointer to move 2 steps
+    let fast = this.head;
+
+    while (fast !== null && fast.next !== null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return slow;
+  }
+}
+
+let myLinkedList = new LinkedList(1);
+myLinkedList.push(2);
+myLinkedList.push(3);
+myLinkedList.push(4);
+myLinkedList.push(5);
+
+console.log('Original list:');
+myLinkedList.printList();
+
+const middleNode = myLinkedList.findMiddleNode();
+console.log(`\nMiddle node value: ${middleNode.value}`);
+
+// Create a new list with an even number of elements
+let myLinkedList2 = new LinkedList(1);
+myLinkedList2.push(2);
+myLinkedList2.push(3);
+myLinkedList2.push(4);
+myLinkedList2.push(5);
+myLinkedList2.push(6);
+
+console.log('\nOriginal list 2:');
+myLinkedList2.printList();
+
+const middleNode2 = myLinkedList2.findMiddleNode();
+console.log(`\nMiddle node value of list 2: ${middleNode2.value}`);
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Original list:
+    1
+    2
+    3
+    4
+    5
+    Middle node value: 3
+    Original list 2:
+    1
+    2
+    3
+    4
+    5
+    6
+    Middle node value of list 2: 4
+*/
---
+++ 02_DSA_Udemy/01_LinkedList/02-questions/QUESTIONS.md
@@ -0,0 +1,50 @@
+## 01 - findMiddleNode
+Implement a member function called findMiddleNode() that finds and returns the middle node of the linked list.
+
+Note: this LinkedList implementation does not have a length member variable.
+
+
+
+Output:
+
+Return the middle node of the linked list.
+
+If the list has an even number of nodes, return the second middle node (the one closer to the end).
+
+
+
+Constraints:
+
+You are not allowed to use any additional data structures (such as arrays) or modify the existing data structure.
+
+You can only traverse the linked list once.
+
+
+
+
+
+Example 1:
+
+Suppose you have a LinkedList object, list, with the following values:
+1 -> 2 -> 3 -> 4 -> 5
+
+
+After calling the findMiddleNode() function:
+
+let middle = list.findMiddleNode();
+The middle node should have the value 3.
+
+
+
+
+
+Example 2:
+
+Now suppose you have a LinkedList object, list, with the following values:
+1 -> 2 -> 3 -> 4 -> 5 -> 6
+
+
+After calling the findMiddleNode() function:
+
+let middle = list.findMiddleNode();
+The middle node should have the value 4.
