7be8b775ef817285643090a6706e1fe5c38e70d8
Sumit Paul <pauls1317@gmail.com>
2023-10-16 13:57:13 +0530

    LL: reverse list

---
+++ 02_DSA_Udemy/01_LinkedList/11_Reverse.js
@@ -0,0 +1,74 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const node = new Node(value);
+    this.head = node;
+    this.tail = node;
+    this.length = 1;
+  }
+
+  push(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  printList() {
+    let temp = this.head;
+    while (temp !== null) {
+      console.log(temp.value);
+      temp = temp.next;
+    }
+  }
+
+  /** reverse the linked list */
+  reverse() {
+    // swapping head and tail node
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+
+    // variables to stpre previous node and next node
+    let prev = null;
+    let next = temp.next;
+
+    // iterate through list
+    for (let i = 0; i < this.length; i++) {
+      // store referance of the next node
+      next = temp.next;
+      // assign current node next to previous node
+      temp.next = prev;
+      // assign previous node to current node
+      prev = temp;
+      // move to next node -> change current node to next node
+      temp = next;
+    }
+    return this;
+  }
+}
+
+function main() {
+  let ll = new LinkedList(1);
+  ll.push(2);
+  ll.push(3);
+  ll.push(4);
+  ll.printList();
+  ll.reverse();
+  ll.printList();
+}
+
+main();
