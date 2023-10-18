66fa1bada5ab19f0e6d398ae5653f5226270a7dd
Sumit Paul <pauls1317@gmail.com>
2023-10-19 00:09:23 +0530

    Queue: Dequeue

---
+++ 02_DSA_Udemy/04_Queue/03_Dequeue.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(value) {
+    const node = new Node(value);
+    this.first = node;
+    this.last = node;
+    this.length = 1;
+  }
+
+  enqueue(value) {
+    let node = new Node(value);
+    if (this.length === 0) {
+      this.first = this.last = node;
+    } else {
+      this.last.next = node;
+      this.last = node;
+    }
+    this.length++;
+    return this;
+  }
+
+  /** remove element from the start of queue */
+  dequeue() {
+    // if empty queue
+    if (!this.first) {
+      return undefined;
+    }
+    // store first pointer referance
+    let temp = this.first;
+
+    // move first to next node
+    this.first = temp.next;
+
+    // remove temp node's pointer
+    temp.next = null;
+
+    this.length--;
+    return temp;
+  }
+}
+
+let q = new Queue(1);
