7d608f425dceafe54c08435c1b780dd6d7855302
Sumit Paul <pauls1317@gmail.com>
2023-10-18 22:48:12 +0530

    Stack: Push

---
+++ 02_DSA_Udemy/03_Stack/02_Push.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(value) {
+    const node = value ? new Node(value) : null;
+    this.top = node;
+    this.length = value ? 1 : 0;
+  }
+
+  /** method to add elements in the Stack */
+  // NOTE - Elements can be added only at start of the Stack
+  push(value) {
+    // create node with given value
+    let node = new Node(value);
+    // add referance of top of the stack to next of the new node
+    node.next = this.top;
+    // new node becames the top -> FIFO
+    this.top = node;
+
+    this.length++;
+    return this;
+  }
+}
+
+let myStack = new Stack(null);
+
+myStack.push(1);
+myStack.push(2);
