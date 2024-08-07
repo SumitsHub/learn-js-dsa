27ee6fe604ec2af2dcafbeb325460e04c146650c
Sumit Paul <pauls1317@gmail.com>
2024-08-07 09:21:35 +0530

    class file - comment

--- 02_DSA_Udemy/class.js
+++ 02_DSA_Udemy/class.js
@@ -1,3 +1,5 @@
+/** This is just JavaScript Class Revision */
+
 class Cookie {
   constructor(color) {
     this.color = color;
@@ -10,7 +12,7 @@ class Cookie {
   }
 }
 
-let cookieOne = new Cookie('green');
-let cookieTwo = new Cookie('blue');
+let cookieOne = new Cookie("green");
+let cookieTwo = new Cookie("blue");
 
 const list = Map();
